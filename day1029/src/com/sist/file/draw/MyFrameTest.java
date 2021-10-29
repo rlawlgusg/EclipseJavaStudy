76be4a642ae22ac9ca9a9cf51cc7fee9c7aa697f
rlawlgusg <iiillliiiili@naver.com>
2021-10-30 00:32:56 +0900

    day1029

---
+++ day1029/src/com/sist/file/draw/MyFrameTest.java
@@ -0,0 +1,11 @@
+package com.sist.file.draw;
+
+public class MyFrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyFrame f = new MyFrame();
+	}
+
+}
