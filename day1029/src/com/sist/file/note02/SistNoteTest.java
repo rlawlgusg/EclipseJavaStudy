897605ae01651f440b53f2e251be7fe70fb09954
rlawlgusg <iiillliiiili@naver.com>
2021-10-31 01:07:31 +0900

    day1029 추가

---
+++ day1029/src/com/sist/file/note02/SistNoteTest.java
@@ -0,0 +1,10 @@
+package com.sist.file.note02;
+
+public class SistNoteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new SistNote();
+	}
+
+}
---
+++ day1029/src/com/sist/file/note04/FileDeleteTest.java
@@ -0,0 +1,16 @@
+package com.sist.file.note04;
+
+import java.io.File;
+
+public class FileDeleteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//"c:/myData/메모.txt"파일을 삭제하는 프로그램을 작성
+		File file = new File("c:/myData/메모.txt");
+		file.delete();
+		System.out.println("삭제하였습니다");
+	}
+
+}
